e765da39c70794e5dc7d0c62c7616ac4633f99b5
Andre Cruz <ajcruzg95@gmail.com>
2023-07-14 16:59:15 -0500

    first commit

---
+++ micro-admin-config-server/src/main/java/com/tutorial/springboot/app/MicroAdminConfigServerApplication.java
@@ -0,0 +1,14 @@
+package com.tutorial.springboot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+@EnableConfigServer
+@SpringBootApplication
+public class MicroAdminConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroAdminConfigServerApplication.class, args);
+	}
+
+}
